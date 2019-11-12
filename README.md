1a7b5435d33843958d96e13740f2afb2d69319d3
Oussema TOUKEBRI <oussema.toukebri.ext@spw.wallonie.be>
2019-11-12 13:31:00 +0100

    add security repository

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # orders-microservice
 orders-microservice
+
+# configuration
+## init security
+
+INSERT INTO roles(name) VALUES('ROLE_USER');
+INSERT INTO roles(name) VALUES('ROLE_ADMIN');
--- src/main/java/tn/zelda/projects/microservice/ordermicroservice/OrderMicroserviceApplication.java
+++ src/main/java/tn/zelda/projects/microservice/ordermicroservice/OrderMicroserviceApplication.java
@@ -1,6 +1,7 @@
 package tn.zelda.projects.microservice.ordermicroservice;
 
 import java.util.Set;
+import java.util.TimeZone;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,7 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -18,50 +21,61 @@ import tn.zelda.projects.microservice.ordermicroservice.domain.Order;
 import tn.zelda.projects.microservice.ordermicroservice.domain.OrderDetail;
 import tn.zelda.projects.microservice.ordermicroservice.domain.Product;
 
+import javax.annotation.PostConstruct;
+
+@EntityScan(basePackageClasses = {
+        OrderMicroserviceApplication.class,
+        Jsr310JpaConverters.class
+})
 @SpringBootApplication
 public class OrderMicroserviceApplication implements CommandLineRunner {
 
-	private static final Logger log = LoggerFactory.getLogger(OrderMicroserviceApplication.class);
+    private static final Logger log = LoggerFactory.getLogger(OrderMicroserviceApplication.class);
 
-	@Autowired
-	private OrderRepository orderRepository;
+    @Autowired
+    private OrderRepository orderRepository;
 
-	@Autowired
-	private ProductRepository productRepository;
+    @Autowired
+    private ProductRepository productRepository;
 
-	public static void main(String[] args) {
-		SpringApplication.run(OrderMicroserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMicroserviceApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
+    @PostConstruct
+    void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 
-		log.debug("OrderMicroservice command line runner!");
+    @Override
+    public void run(String... args) throws Exception {
 
-		//init phase normally product should already exits in db
-		Product product = new Product("Microservices", "ms_ebook", 50);
-		productRepository.save(product);
+        log.debug("OrderMicroservice command line runner!");
+
+        //init phase normally product should already exits in db
+        Product product = new Product("Microservices", "ms_ebook", 50);
+        productRepository.save(product);
 
 
-		Order order = new Order();
-		Set<OrderDetail> odSet = order.getOrdersDetails();
-		System.out.println(odSet);
-		odSet.add(new OrderDetail(product, order));
-		try {
-			orderRepository.save(order);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+        Order order = new Order();
+        Set<OrderDetail> odSet = order.getOrdersDetails();
+        System.out.println(odSet);
+        odSet.add(new OrderDetail(product, order));
+        try {
+            orderRepository.save(order);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-	}
+    }
 
-	@Bean
-	CommandLineRunner runner() {
-		return args -> {
+    @Bean
+    CommandLineRunner runner() {
+        return args -> {
 
-			WebClient.create("http://localhost:8080").method(HttpMethod.GET).retrieve().bodyToMono(String.class);
+            WebClient.create("http://localhost:8080").method(HttpMethod.GET).retrieve().bodyToMono(String.class);
 
-			System.out.println("CommandLineRunner running in the UnsplashApplication class...");
-		};
-	}
+            System.out.println("CommandLineRunner running in the UnsplashApplication class...");
+        };
+    }
 }
